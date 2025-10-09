39bbf771aec3c8409c53690d37315b72efe0c001
user <ashirov.babur13@gmail.com>
2025-10-10 00:24:49 +0500

    added hello 2 2.5 Итоги пройденных тем. Проверьте себя 1+2+3

--- python_learning.py
+++ python_learning.py
@@ -2,13 +2,64 @@ print ('Hello World')
 print('my name Bobur')
 print('timur say "hello"')
 print('Hello World')
-
 print ('Hello 1 World')
-
 print("Hello World")
-
 print('Hello')
-
 print("'Привет',")
 print('Меня зовут Вася')
 print ('и я умнее всех')
+
+izi ='«Моя первая переменная!»'
+print (izi)
+win ='«И не последняя!»'
+print  (win)
+
+name = 'Иван'
+my_color = 'Зелёный'
+sport = 'Бег'
+print('Меня зовут')
+print(name)
+print('Мой любимый цвет:')
+print(my_color)
+print('Мой любимый вид спорта:')
+print(sport)
+
+
+name = input('Введите имя: ')
+print("Hello " + name )
+name2  = input('Введите фамилию: ')
+print("Hello " + name2 + ' ' + name + ' я у тебя за спиной!!!')
+
+
+
+
+sur_name = input ('Введите фамилию: ')
+print('Ваша фамилия - ' + sur_name)
+
+
+name = input('Введите имя: ')
+print("Hello, " + name )
+
+name = input('Введите имя: ')
+print("Hello! , " + name )
+
+name = input('Введите первое слова: ')
+name2 = input('Введите второе слова: ')
+print( name + name2)
+
+client = 'Петя'
+pet = 'кот'
+print(client + ' и ' + pet)
+
+
+r = 'Red'
+g = 'Green'
+b = 'Blue'
+
+print(r, b, g, r+g+b, b, g+b)
+
+
+
+
+
+
