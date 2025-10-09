d249a9015d62780e2417adf424b68ab5b2abb162
user <ashirov.babur13@gmail.com>
2025-10-09 23:29:45 +0500

    added hello 2

--- python_learning.py
+++ python_learning.py
@@ -1,1 +1,14 @@
-print("hello world")
+print ('Hello World')
+print('my name Bobur')
+print('timur say "hello"')
+print('Hello World')
+
+print ('Hello 1 World')
+
+print("Hello World")
+
+print('Hello')
+
+print("'Привет',")
+print('Меня зовут Вася')
+print ('и я умнее всех')
