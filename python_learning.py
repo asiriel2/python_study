ac5073261fcb0f5f7504c153b3c07b1cc3fe7fac
user <ashirov.babur13@gmail.com>
2025-10-11 15:40:43 +0500

    added hello 2 1-2 пройдет и практические работы сделаны вопрос в 5 и 7 практической задаче

--- python_learning.py
+++ python_learning.py
@@ -25,23 +25,17 @@ print('Мой любимый вид спорта:')
 print(sport)
 
 
-name = input('Введите имя: ')
+name = input( 'Введите имя: ')
 print("Hello " + name )
 name2  = input('Введите фамилию: ')
 print("Hello " + name2 + ' ' + name + ' я у тебя за спиной!!!')
 
-
-
-
 sur_name = input ('Введите фамилию: ')
 print('Ваша фамилия - ' + sur_name)
-
 
 name = input('Введите имя: ')
 print("Hello, " + name )
 
-name = input('Введите имя: ')
-print("Hello! , " + name )
 
 name = input('Введите первое слова: ')
 name2 = input('Введите второе слова: ')
@@ -58,8 +52,40 @@ b = 'Blue'
 
 print(r, b, g, r+g+b, b, g+b)
 
+aninal1 = input('first animal: ')
+animal2  = input('second animal: ')
+print( aninal1 + ' спит, ' + animal2 + ' идёт')
+
+name = input('Введите имя: ')
+name2  = input('Введите фамилию: ')
+print( "Вас зовут: ")
+print( name)
+print(name2)
+
+name = input('Введите имя: ')
+name2  = input('Введите фамилию: ')
+city3 = input( 'введите город проживания: ')
+print('===============================')
+print( "Вас зовут: " + name + ' ' + name2 )
+print( 'Вы живете в городе: ' + city3 )
+
+first_name = input('Введите имя пользователя: ')
+print ( 'Утро доброе  ' + first_name)
+print("К сожалению, у Вас нет доступа к системе.")
+print("Пожалуйста, обратитесь к системному администратору.")
+
+city = input ( 'город вылета: ')
+city2 = input ( 'город прилёта: ')
+print ('из ' + city + ' - в - ' + city2 + '.')
+
+a = input('Введите первое слово: ')
+b = input('Введите второе слово: ')
+print(a, b)
+a = b
+b = a
+print(a, b)
 
 
-
-
-
+user = input ( 'your name: ')
+new_file  = input ( 'name file: ')
+print ('D:/STUDYING/123.txt')
