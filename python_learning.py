6423a23053b64db01e59b119134ed9b0e694146f
user <ashirov.babur13@gmail.com>
2025-10-13 23:14:01 +0500

    added hello 2 1-2 пройдет и практические работы сделаны вопрос в 5 и 7 практической задаче

--- python_learning.py
+++ python_learning.py
@@ -1,91 +1,253 @@
-print ('Hello World')
-print('my name Bobur')
-print('timur say "hello"')
-print('Hello World')
-print ('Hello 1 World')
-print("Hello World")
-print('Hello')
-print("'Привет',")
-print('Меня зовут Вася')
-print ('и я умнее всех')
+# # print ('Hello World')
+# # print('my name Bobur')
+# # print('timur say "hello"')
+# # print('Hello World')
+# # print ('Hello 1 World')
+# # print("Hello World")
+# # print('Hello')
+# # print("'Привет',")
+# # print('Меня зовут Вася')
+# # print ('и я умнее всех')
+# #
+# # izi ='«Моя первая переменная!»'
+# # print (izi)
+# # win ='«И не последняя!»'
+# # print  (win)
+# #
+# # name = 'Иван'
+# # my_color = 'Зелёный'
+# # sport = 'Бег'
+# # print('Меня зовут')
+# # print(name)
+# # print('Мой любимый цвет:')
+# # print(my_color)
+# # print('Мой любимый вид спорта:')
+# # print(sport)
+# #
+# #
+# # name = input( 'Введите имя: ')
+# # print("Hello " + name )
+# # name2  = input('Введите фамилию: ')
+# # print("Hello " + name2 + ' ' + name + ' я у тебя за спиной!!!')
+# #
+# # sur_name = input ('Введите фамилию: ')
+# # print('Ваша фамилия - ' + sur_name)
+# #
+# # name = input('Введите имя: ')
+# # print("Hello, " + name )
+# #
+# #
+# # name = input('Введите первое слова: ')
+# # name2 = input('Введите второе слова: ')
+# # print( name + name2)
+# #
+# # client = 'Петя'
+# # pet = 'кот'
+# # print(client + ' и ' + pet)
+# #
+# #
+# # r = 'Red'
+# # g = 'Green'
+# # b = 'Blue'
+# #
+# # print(r, b, g, r+g+b, b, g+b)
+# #
+# # aninal1 = input('first animal: ')
+# # animal2  = input('second animal: ')
+# # print( aninal1 + ' спит, ' + animal2 + ' идёт')
+# #
+# # name = input('Введите имя: ')
+# # name2  = input('Введите фамилию: ')
+# # print( "Вас зовут: ")
+# # print( name)
+# # print(name2)
+# #
+# # name = input('Введите имя: ')
+# # name2  = input('Введите фамилию: ')
+# # city3 = input( 'введите город проживания: ')
+# # print('===============================')
+# # print( "Вас зовут: " + name + ' ' + name2 )
+# # print( 'Вы живете в городе: ' + city3 )
+# #
+# # first_name = input('Введите имя пользователя: ')
+# # print ( 'Утро доброе  ' + first_name)
+# # print("К сожалению, у Вас нет доступа к системе.")
+# # print("Пожалуйста, обратитесь к системному администратору.")
+# #
+# # city = input ( 'город вылета: ')
+# # city2 = input ( 'город прилёта: ')
+# # print ('из ' + city + ' - в - ' + city2 + '.')
+#
+# # first_name = input('Введите имя пользователя: ')
+# # greeting = 'Утро доброе'
+# # print(greeting, first_name)
+# # intro = "К сожалению, у Вас нет доступа к системе."
+# # info = "Пожалуйста, обратитесь к системному администратору."
+# # print(intro)
+# # print(info)
+#
+# # a = input('Введите первое слово: ')
+# # b = input('Введите второе слово: ')
+# # print(a, b)
+# # a = b
+# # b = a
+# # print(a, b)
+#
+# user = input ( 'your name: ')
+# new_file  = input ( 'name file: ')
 
-izi ='«Моя первая переменная!»'
-print (izi)
-win ='«И не последняя!»'
-print  (win)
 
-name = 'Иван'
-my_color = 'Зелёный'
-sport = 'Бег'
-print('Меня зовут')
-print(name)
-print('Мой любимый цвет:')
-print(my_color)
-print('Мой любимый вид спорта:')
-print(sport)
+# =================================================================
+# print(3**5)
+# print(8/-4)
+# print(10/2+6)
 
+# a=5
+# b=4
+# c=2
+# print('сложение:', a+b+c)
+# print('вычитание:', a-b-c)
+# print('умножение:', a*b*c)
+# print('деление:', a/b/c)
+# print('степень:', a**b**c)
 
-name = input( 'Введите имя: ')
-print("Hello " + name )
-name2  = input('Введите фамилию: ')
-print("Hello " + name2 + ' ' + name + ' я у тебя за спиной!!!')
-
-sur_name = input ('Введите фамилию: ')
-print('Ваша фамилия - ' + sur_name)
-
-name = input('Введите имя: ')
-print("Hello, " + name )
+# a=8
+# b=5
+# c=3
+# print((a/(b+c)-a)/c)
 
 
-name = input('Введите первое слова: ')
-name2 = input('Введите второе слова: ')
-print( name + name2)
+# bus = 5
+# metro = 3
+# result = (6+((10 - bus)**2))/(metro + 1) + (132 / (2 + bus))
+# print(result)
 
-client = 'Петя'
-pet = 'кот'
-print(client + ' и ' + pet)
+# a=3
+# b=8**2
+# c=12
+# d=4**3**2
+# f=2
+# g=18
+#
+# result =(-a+(b-c)*d) / (f*g)
+# print(result)
 
+# a=int(input("введите первое число:"))
+# b=int(input("введите второе число:"))
+# result = a+b
+# print('сумма:', result)
 
-r = 'Red'
-g = 'Green'
-b = 'Blue'
-
-print(r, b, g, r+g+b, b, g+b)
-
-aninal1 = input('first animal: ')
-animal2  = input('second animal: ')
-print( aninal1 + ' спит, ' + animal2 + ' идёт')
-
-name = input('Введите имя: ')
-name2  = input('Введите фамилию: ')
-print( "Вас зовут: ")
-print( name)
-print(name2)
-
-name = input('Введите имя: ')
-name2  = input('Введите фамилию: ')
-city3 = input( 'введите город проживания: ')
-print('===============================')
-print( "Вас зовут: " + name + ' ' + name2 )
-print( 'Вы живете в городе: ' + city3 )
-
-first_name = input('Введите имя пользователя: ')
-print ( 'Утро доброе  ' + first_name)
-print("К сожалению, у Вас нет доступа к системе.")
-print("Пожалуйста, обратитесь к системному администратору.")
-
-city = input ( 'город вылета: ')
-city2 = input ( 'город прилёта: ')
-print ('из ' + city + ' - в - ' + city2 + '.')
-
-a = input('Введите первое слово: ')
-b = input('Введите второе слово: ')
-print(a, b)
-a = b
-b = a
-print(a, b)
+# a=int(input("введите первое число:"))
+# b=int(input("введите второе число:"))
+# c=int(input("введите третье число:"))
+# d=int(input("введите четвертое число:"))
+# result = 2*(c+5+((a*b)/(4*b)))*(d-2*((a**3)/30))-10
+# print('Ответ:', result)
 
 
-user = input ( 'your name: ')
-new_file  = input ( 'name file: ')
-print ('D:/STUDYING/123.txt')
+#
+# a = '2'
+# b = '5'
+# c = '3'
+# num =  6 ** int(a) + (7 - int(b)) * int(c)
+# print(num)
+
+# apple = 41
+# box = apple /3
+# boxs = apple // 3
+# print('всего ящиков: ', boxs)
+# print('остаток яблок: ', apple % boxs)
+#
+# chislo =int(input("Введите число: "))
+# chislo =496
+# print('номер дома:',chislo%10)
+# print('номер квартиры:', chislo//10)
+
+
+
+
+
+
+# # Practica
+# a = 8
+# b = 10
+# c = 12
+# d = 18
+# res=((-3+a**2)*b-2**3)/(c-2*d)
+# print(res)
+
+# a=int(input("Enter a number:"))
+# b=int(input("Enter b number:"))
+# c=int(input("Enter c number:"))
+# d=int(input("Enter d number:"))
+# e=a+b
+# f=c+d
+# g=e/f
+# print('summa a+b=', e)
+# print('summa c+d=', f)
+# print('summa (a+b)/(c+d)=',g)
+
+# number =int(input("Введите число: "))
+# bolshe = number + 1
+# menshe = number - 1
+# print('после числа', number ,'идет число',  bolshe )
+# print('до числа', number ,'идет число', menshe )
+
+# storona_a =int(input("Введите длину стороны a: "))
+# storona_b =int(input("Введите длину стороны b: "))
+# otvet = storona_a*storona_b/2
+# print('длина двух катетов в прямоугольном треугольнике равно:', otvet)
+
+# minuta =int(input("укажиете кол-во минут : "))
+# chas=minuta//60
+# ostatok= minuta%60
+# print('в часах это:' ,chas)
+# print('остаток минут:', ostatok)
+
+
+# pervoe = int(input("Введите первое 3х значное число: "))
+# vtoroe = int(input("Введите второе 3х значное число: "))
+# print( (pervoe%100) + (vtoroe%100))
+
+# chislo =int(input("Введите первое 4х значное число: "))
+# chislo = 4567
+# pervoe = chislo//1000
+# vtoroe = chislo%1000//100
+# tretiy = chislo%100//10
+# chetvertiy = chislo%10
+# print(pervoe)
+# print(vtoroe)
+# print(tretiy)
+# print(chetvertiy)
+
+
+
+# a = int(input('Введите первое число: '))
+# b = int(input('Введите второе число: '))
+a = 1 # 5
+b = 2 # 1
+c = 3 # 2
+d = 4 # 3
+e = 5 # 4
+print(a, b, c, d, e)
+a=1+2+3+4+5
+b=a-b-c-d-e
+c=a-b-c-d-e
+d=a-b-c-d-e
+e=a-b-c-d-e
+a=
+print(a, b, c, d, e)
+a=1+2+3+4+5
+
+
+
+
+
+
+
+
+
+
+
+
+
